72c70df548c836cfb8d5ae90ceda759a3633ac0b
Amir Zamani <amirhoseinzamani.mv@gmail.com>
2026-07-07 14:38:18 +0330

    code_b

--- Remote.c
+++ Remote.c
@@ -1,4 +1,5 @@
 //start of pro remot code with ability of learn and ..
 //work with FMD
 //company :sepahbod by amirhosein zamani 
-//this code was compiled in FMD IDE for own company of FMD copiler
+//this code was compiled in FMD IDE for own company of FMD compiler
+//now we are  in new branch code 
