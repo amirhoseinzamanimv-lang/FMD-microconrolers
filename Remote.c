e23cbf55916832968109f7fa81ae431cea66315b
Amir Zamani <amirhoseinzamani.mv@gmail.com>
2026-07-07 13:19:27 +0330

    define copiler

--- Remote.c
+++ Remote.c
@@ -1,4 +1,4 @@
 //start of pro remot code with ability of learn and ..
 //work with FMD
 //company :sepahbod by amirhosein zamani 
-//this code was compiled in FMD IDE for own company of FMD
+//this code was compiled in FMD IDE for own company of FMD copiler
